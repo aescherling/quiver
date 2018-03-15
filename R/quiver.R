874165cf3967ea0480b9cea28bfb98c98e5265ec
aescherling <adam.scherling@gmail.com>
2018-03-15 08:03:02 -0700

    moved js, css files to inst directory

--- R/quiver.R
+++ R/quiver.R
@@ -5,20 +5,20 @@
 library(glue)
 
 # load templates
-uhist_template <- readChar('uhist_template.html', file.info('uhist_template.html')$size)
-mplot_template <- readChar('mplot_template.html', file.info('mplot_template.html')$size)
-iview_template <- readChar('iview_template.html', file.info('iview_template.html')$size)
+uhist_template <- readChar('inst/uhist_template.html', file.info('inst/uhist_template.html')$size)
+mplot_template <- readChar('inst/mplot_template.html', file.info('inst/mplot_template.html')$size)
+iview_template <- readChar('inst/iview_template.html', file.info('inst/iview_template.html')$size)
 
 # load dependencies
-bootstrap <- readChar('css/bootstrap.min.css', file.info('css/bootstrap.min.css')$size)
-d3 <- readChar('lib/d3.v4.min.js', file.info('lib/d3.v4.min.js')$size)
-crossfilter <- readChar('lib/crossfilter.min.js', file.info('lib/crossfilter.min.js')$size)
-queue <- readChar('lib/queue.min.js', file.info('lib/queue.min.js')$size)
+bootstrap <- readChar('inst/css/bootstrap.min.css', file.info('inst/css/bootstrap.min.css')$size)
+d3 <- readChar('inst/lib/d3.v4.min.js', file.info('inst/lib/d3.v4.min.js')$size)
+crossfilter <- readChar('inst/lib/crossfilter.min.js', file.info('inst/lib/crossfilter.min.js')$size)
+queue <- readChar('inst/lib/queue.min.js', file.info('inst/lib/queue.min.js')$size)
 
 # load quiver javascript files
-uhist <- readChar('uhist.js', file.info('uhist.js')$size)
-mplot <- readChar('mplot.js', file.info('mplot.js')$size)
-iview <- readChar('iview.js', file.info('iview.js')$size)
+uhist <- readChar('inst/uhist.js', file.info('inst/uhist.js')$size)
+mplot <- readChar('inst/mplot.js', file.info('inst/mplot.js')$size)
+iview <- readChar('inst/iview.js', file.info('inst/iview.js')$size)
 
 # glue them together
 uhist.html <- glue(uhist_template)
